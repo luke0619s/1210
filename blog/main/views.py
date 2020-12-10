ae79777f81ec3a37b4e2c7bd8b88b49562c982e0
luke <950203@nkps.tp.edu.tw>
2020-12-10 19:42:48 +0800

    首次前端

--- blog/main/views.py
+++ blog/main/views.py
@@ -1,5 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 def main(request):
-    return HttpResponse('這是輔仁資訊-登革熱病媒蚊監測系統組')
+    return render(request, 'index.html', locals())
