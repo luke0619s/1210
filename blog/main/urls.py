32e28f0296a6d55452c6a9cde43b429d1660e5eb
luke <950203@nkps.tp.edu.tw>
2020-12-10 20:15:46 +0800

    前端大更新

--- blog/main/urls.py
+++ blog/main/urls.py
@@ -3,5 +3,7 @@ from main import views
 
 app_name = 'main'
 urlpatterns = [
-    path('', views.main, name='main')
+    path('', views.main, name='main'),
+    path('station/', views.station, name='station'),
+    path('sort/', views.sort, name='sort'),
     ]
--- blog/main/views.py
+++ blog/main/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 def main(request):
     return render(request, 'main/index.html')
+def station(request):
+    return render(request, 'main/index.html')
+def sort(request):
+    return render(request, 'main/index.html')
