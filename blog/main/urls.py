251a06a381f05904e06167418d0f846ddfa2bb56
luke <950203@nkps.tp.edu.tw>
2020-12-16 18:51:00 +0800

    大更動

--- blog/main/urls.py
+++ blog/main/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('station/', views.station, name='station'),
     path('sort/', views.sort, name='sort'),
+    path('sign/', views.sign, name='sign'),
+    path('station_history/', views.station_history, name='station_history'),
+    path('setting/', views.setting, name='setting'),
+    path('upgrade_member/', views.upgrade_member, name='upgrade_member'),
+    path('government_data/',views.government, name='government_data'),
+    path('realtime/',views.realtime, name='realtime'),
     ]
--- blog/main/views.py
+++ blog/main/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render
 def main(request):
     return render(request, 'main/index.html')
 def station(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/station.html')
 def sort(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/sort.html')
+def sign(request):
+    return render(request, 'main/sign.html')
+def station_history(request):
+    return render(request, 'main/station_history.html')
+def setting(request):
+    return render(request, 'main/setting.html')
+def upgrade_member(request):
+    return render(request, 'main/upgrade_member.html')
+def government(request):
+    return render(request, 'main/government_data.html')
+def realtime(request):
+    return render(request, 'main/realtime.html')
